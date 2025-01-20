15c1af26e4b02a8c967cf127b3aae0d5ca15d556
Eyugecho <eyuti6834@gmail.com>
2025-01-20 21:23:16 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+// import Header from "./components/Header";
+// import Home from "./pages/Home";
+import { RouterProvider } from "react-router";
+
+// import Footer from "./components/Footer";
+
+
+const Layout = () => {
+  return (
+    <>
+      {/* <Header /> */}
+      <Outlet />
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <></>,
+    children: [
+      {
+        path: "/",
+        // element: <Home />,
+      },
+  
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
